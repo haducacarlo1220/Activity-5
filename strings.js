bb3dbd332d15e13e654bb5db5701bf558759be66
haducacarlo1220 <ralphhaduca@gmail.com>
2024-09-15 09:48:04 +0800

    Initial commit with strings.js

---
+++ strings.js
@@ -0,0 +1,14 @@
+let favActorsFirstName = "Ryan";
+let favActprsLastNAme = "Reynolds";
+let fullName = favActorsFirstName + " " + favActprsLastNAme;
+console.log(favActorsFirstName);
+console.log(favActprsLastNAme);
+console.log(fullName);
+
+let upperCase = "ryan reynolds";
+console.log(upperCase.toUpperCase());
+let lowerCase = "AKHIRA";
+console.log(lowerCase.toUpperCase());
+
+let message = "My favorite actor is " + (fullName.toUpperCase()) + " his best show is Dead Pool";
+console.log(message);
